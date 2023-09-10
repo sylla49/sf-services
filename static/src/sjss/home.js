025c3fdb91031cb3f8e4f1b463c4a935bbd379a0
sylla49 <sas.solution.49@gmail.com>
2023-09-10 21:58:39 +0200

    container update

--- static/src/sjss/home.js
+++ static/src/sjss/home.js
@@ -1,21 +1,50 @@
 'use strict';
 
 $().ready( ()=>{
+
+	// window width and Height
 	let hauteur = $(window).height();
 	let wlager = $(window).width();
-	let userAgent = navigator.userAgent;
 
-	// alert("Largeur "+ wlager +"  Hauteur "+ hauteur+"  "+userAgent);
+    /*
+    {
+          var OSName = "Unknown OS",
+          if (navigator.userAgent.indexOf("Win") != -1) OSName = "Windows";
+          if (navigator.userAgent.indexOf("Mac") != -1) OSName = "Macintosh";
+          if (navigator.userAgent.indexOf("Linux") != -1) OSName = "Linux";
+          if (navigator.userAgent.indexOf("Android") != -1) OSName = "Android";
+          if (navigator.userAgent.indexOf("like Mac") != -1) OSName = "iOS";
+          console.log('Your OS: ' + OSName);
+    }
+    */
 
-	const firefox = () =>{
-		let userAgent = navigator.userAgent;
+    // Browser infortion detecting 
+    const browser = {
+    	navVer: navigator.appVersion,
+    	navCod: navigator.appCodeName,
+    	naCook: navigator.cookieEnabled,
+    	sysNam: navigator.platform,
+        navAgt: navigator.userAgent,
+        navNam: navigator.appName,
 
-		if ( $(window).width() <= 412 || $(window).width() <= 414 ){
-			$('.dv-ul1').hide();
+        checkBrowser: ()=>{
+        	alert("App Version "+browser.navVer+"\n"+"userAgent "+browser.navAgt+"\n"+"Platform "+browser.sysNam);
+        }
+    };
+    browser.checkBrowser();
 
-			// alert("Note 20 Ultra 5G");
-		}
-	};
-	firefox();
+
+    // Some body moditfications
+
+    let mlogo = $('#imgs video'),
+        img   = $('#imgs img');
+
+    let change = setTimeout(()=>{
+    	mlogo.slideUp("slow");
+    	setTimeout(()=>{
+    		// img.slideDown("fast");
+    		img.toggle("slow");
+    	}, 500);
+    }, 3000);
 
 } );
